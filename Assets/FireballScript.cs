d916cfe7ef91d6dd5dda5bb8745293cfaf6a61da
KuhnChris <kuhnchris@kuhnchris.eu>
2019-04-23 18:12:04 +0200

    Added FireballScript for moving the fireball (prefab preparation)

---
+++ Assets/FireballScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    private Transform t;
+    public float speedX = 0.01f;
+    public float speedY = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = this.GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 p = t.position;
+        p.x += speedX;
+        p.y += speedY;
+        t.position = p;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
